bdb2d69fd69a112ba513d7721d44e4a1d255e53f
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-02-06 16:54:49 +0000

    Initial commit of improved pre-Byblo filtering code from Julie

---
+++ discoutils/prebyblo_filter.py
@@ -0,0 +1,112 @@
+import argparse
+import logging
+
+__author__ = 'Julie'
+
+import re, sys
+
+
+def count(filename, pos_patterns):
+    logging.info('Counting entry patterns: %s', [x.pattern for x in pos_patterns])
+    counts = {} #dictionary to hold counts of items
+
+    with open(filename, 'r') as instream:
+        logging.info("Reading %s", filename)
+        linesread = 0
+        for line in instream:
+            initial = line.split('\t')[0]
+            if any(posPATT.match(initial) for posPATT in pos_patterns):
+                current = counts.get(initial, 0)
+                counts[initial] = current + 1
+            linesread += 1
+            if linesread % 10000 == 0:
+                logging.info("Read %d lines", linesread)
+    return counts, linesread
+
+
+def filterline(fields, pattern):
+    #filter out features in exclusion_list
+    if pattern:
+        fields = [f for f in fields if pattern.match(f)]
+        return '%s\n' % ('\t'.join(fields)) if fields else ''
+    else:
+        return '%s\n' % ('\t'.join(fields))
+
+
+def do_filtering(filename, outstream, threshold, pos_patterns, feature_pattern, counts, total_lines):
+    logging.info("Rereading %s", filename)
+    with open(filename, 'r') as instream:
+        linesprocessed = 0
+        for line in instream:
+            line = line.rstrip()
+            fields = line.split('\t')
+            initial = fields.pop(0)
+            if any(p.match(initial) for p in pos_patterns):
+                if counts[initial] > threshold:
+                    fields = filterline(fields, feature_pattern)
+                    if fields:
+                        outstream.write('%s\t%s' % (initial, fields))
+            linesprocessed += 1
+            if linesprocessed % 10000 == 0:
+                percent = linesprocessed * 100. / total_lines
+                logging.info("Processed %d lines (%2.1f percent)", linesprocessed, percent)
+
+
+def read_configuration(args):
+    #first 2 args must be filename and frequency threshold
+    pos_patterns = {'N': re.compile('.*/N'),
+                    'V': re.compile('.*/V'),
+                    'J': re.compile('.*/J'),
+                    'R': re.compile('.*/RB')}
+
+    feature_patterns = {'deps': re.compile('T:.*'),
+                        'wins': re.compile('.*-.*:')}
+
+    def pos_pattern_validator(v):
+        try:
+            return pos_patterns[v]
+        except:
+            raise argparse.ArgumentTypeError("String '%s' does not match required format" % v)
+
+    def feature_pattern_validator(v):
+        try:
+            return feature_patterns[v]
+        except:
+            raise argparse.ArgumentTypeError("String '%s' does not match required format" % v)
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('filename', help='Input file')
+    parser.add_argument('threshold', help='Entry frequency threshold', type=int, default=1)
+
+    parser.add_argument('-pos', required=False, type=pos_pattern_validator, nargs='+',
+                        default={pos_patterns['N']},
+                        help='Entry type to accept. Valid choices are N, V, J, R')
+    parser.add_argument('-feats', required=False, default='deps', type=feature_pattern_validator,
+                        help='Feature type to accept. Valid choices are deps or wins')
+
+    parser.add_argument('-o', '--output', required=False, default=None,
+                        help='Name of output file. Default')
+
+    return parser.parse_args()
+
+
+def main():
+    parameters = read_configuration(sys.argv)
+    output = parameters.output if parameters.output else '%s.pbfiltered' % parameters.filename
+    logging.info("Writing %s", output)
+
+    counts, total_lines = count(parameters.filename, parameters.pos) # make count dictionary
+    logging.info("Number of counted words is %d", len(counts))
+
+    with open(output) as outfile:
+        do_filtering(parameters.filename, outfile, parameters.threshold,
+                     parameters.pos, parameters.feats, counts, total_lines)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s\t%(module)s.%(funcName)s """
+                               "(line %(lineno)d)\t%(levelname)s : %(""message)s")
+    main()
+
+
---
+++ discoutils/tests/test_prebyblo_filter.py
@@ -0,0 +1,41 @@
+from textwrap import dedent
+import pytest
+import re
+from prebyblo_filter import count, do_filtering
+
+
+@pytest.fixture
+def filename():
+    return 'discoutils/tests/resources/exp6head'
+
+
+def test_count(filename):
+    counts, total = count(filename,
+                          [re.compile('.*/N'), re.compile('.*/V'),
+                           re.compile('.*/J'), re.compile('.*/RB')])
+    assert counts == {'be/V': 3, 'blame/V': 1, 'bad/J': 1, 'scare/V': 1, 'budgetary/J': 1,
+                      'construction/N': 1, 'consideration/N': 1, 'get/V': 1, 'have/V': 1}
+    assert total == 22
+
+    counts, total = count(filename, [re.compile('.*/N')])
+    assert counts == {'construction/N': 1, 'consideration/N': 1}
+    assert total == 22
+
+
+def test_do_filtering(filename):
+    from cStringIO import StringIO
+
+    s = StringIO()
+    counts, total = count('discoutils/tests/resources/exp6head', [re.compile('.*/N')])
+    do_filtering(filename, s, 0, [re.compile('.*/N')], re.compile('T:.*'), counts, total)
+
+    expected = "consideration/N	T:``	T:there	T:have	T:to	T:be	T:budgetary	T:.\n" \
+               "construction/N	T:it	T:be	T:blame	T:on	T:bad	T:.\n"
+    assert s.getvalue() == expected
+
+    s = StringIO()
+    do_filtering(filename, s, 0, [re.compile('.*/N')], re.compile('.+-(HEAD|DEP):.+'), counts, total)
+
+    expected = "consideration/N	amod-DEP:budgetary	cop-DEP:be	xcomp-HEAD:have	aux-DEP:to\n" \
+               "construction/N	amod-DEP:bad	pobj-HEAD:on\n"
+    assert s.getvalue() == expected
