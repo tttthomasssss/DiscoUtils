08221c1ae65cb3d72028d1f9b0b77f380ca26adc
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-02-06 17:01:53 +0000

    Added setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='DiscoUtils',
+    version='0.1',
+    packages=['discoutils', 'discoutils.tests'],
+    author=['Julie Weeds', 'Miroslav Batchkarov'],
+    author_email=['J.E.Weeds@sussex.ac.uk', 'M.Batchkarov@sussex.ac.uk'],
+    install_requires=['pytest']
+)
+
