c5dab8cf064d420b616f7b7360916b84c0923ad1
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-02-07 18:54:40 +0000

    Added code to find ANs and NNs in FET output

---
+++ discoutils/tests/test_find_all_NPs.py
@@ -0,0 +1,17 @@
+__author__ = 'miroslavbatchkarov'
+from discoutils.find_all_NPs import go
+from cStringIO import StringIO
+
+
+def test_find_all_NPs():
+    s = StringIO()
+    go('discoutils/tests/resources/exp10head.pbfiltered', s)
+    expected = "AN:full-time/J_tribunal/N\n" \
+               "AN:ordinary/J_session/N\n" \
+               "NN:council/N_session/N\n" \
+               "AN:large/J_cat/N\n" \
+               "AN:fluffy/J_cat/N\n" \
+               "NN:house/N_cat/N\n" \
+               "NN:street/N_cat/N\n"
+    print '\n', s.getvalue()
+    assert s.getvalue() == expected
