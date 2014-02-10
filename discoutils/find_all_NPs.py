b53ff0e669260ccf934278819aa537a28d6350dd
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-02-10 17:42:59 +0000

    Added code for building observed window vectors for NPs from FET output

--- discoutils/find_all_NPs.py
+++ discoutils/find_all_NPs.py
@@ -11,10 +11,73 @@ Requires features to have an associated PoS tag, see
 discoutils/tests/resources/exp10head.pbfiltered
 '''
 
-noun_pattern = re.compile('^(\S+?/N).+')  # a noun, non-greedy
-an_pattern = re.compile('amod-DEP:(\S+?/J)')  # an adjective in an amod relation, consisting of non-whitespace
+noun_pattern = re.compile('^(\S+?/N).+')  # a noun entry
+adj_pattern = re.compile('^(\S+?/J).+')  # an adjective entry
 
-nn_pattern = re.compile('nn-DEP:(\S+?/N)')  # an noun in a nn relation
+# an adjective modifier in an amod relation, consisting of non-whitespace
+an_modifier_feature_pattern = re.compile('.*?amod-DEP:(\S+/J).*?')
+an_head_feature_pattern = re.compile('.*?amod-HEAD:(\S+/N).*?')  # an adjective modifier/head in an amod relation
+
+nn_modifier_feature_pattern = re.compile('.*?nn-DEP:(\S+/N).*?')  # an noun modifier in a nn relation
+nn_head_feature_pattern = re.compile('.*?nn-HEAD:(\S+/N).*?')  # an noun head in a nn relation
+
+window_feature_pattern = re.compile('.*?(T:\S+.*?)')  # an noun in a nn relation
+
+
+def _find_and_output_features(head, line, modifier, np_type, outstream, modifier_set):
+    if np_type and head and modifier:
+        # if we've found en entry having the right PoS and dep features, get its window features
+        features = window_feature_pattern.findall(line)
+        if features and modifier in modifier_set:
+            outstream.write('{}:{}_{}\t{}\n'.format(np_type, modifier, head, '\t'.join(features)))
+
+
+def go_get_vectors(infile, outstream, seed_set=ContainsEverything()):
+    with open(infile) as inf:
+        for i, line in enumerate(inf):
+            if i % 100000 == 0:
+                logging.info('Done %d lines', i)
+
+            np_type, head, modifier = None, None, None
+            # check if the entry is an adj or a noun
+            noun_match = noun_pattern.match(line)
+            adj_match = adj_pattern.match(line)
+            amod_head_match = an_head_feature_pattern.match(line)
+            amod_modifier_match = an_modifier_feature_pattern.match(line)
+            nn_head_match = nn_head_feature_pattern.match(line)
+            nn_modifier_match = nn_modifier_feature_pattern.match(line)
+
+            if adj_match and amod_modifier_match:
+                logging.warn('Adjective has adjectival modifiers')
+                np_type = None
+                continue
+
+            if adj_match and amod_head_match:
+                modifier = adj_match.groups()[0]
+                head = amod_head_match.groups()[0]
+                np_type = 'AN'
+                print modifier, head
+                _find_and_output_features(head, line, modifier, np_type, outstream, seed_set)
+
+            if noun_match and amod_modifier_match:
+                head = noun_match.groups()[0]
+                np_type = 'AN'
+                for modifier in an_modifier_feature_pattern.findall(line):
+                    _find_and_output_features(head, line, modifier, np_type, outstream, seed_set)
+
+            if noun_match and nn_head_match:
+                np_type = 'NN'
+                head = noun_match.groups()[0]
+                modifier = nn_head_match.groups()[0]
+                print head, modifier
+                _find_and_output_features(head, line, modifier, np_type, outstream, seed_set)
+
+            if noun_match and nn_modifier_match:
+                np_type = 'NN'
+                head = nn_modifier_match.groups()[0]
+                for modifier in nn_modifier_feature_pattern.findall(line):
+                    print head, modifier
+                    _find_and_output_features(head, line, modifier, np_type, outstream, seed_set)
 
 
 def go(infile, outstream, seed_set=ContainsEverything()):
@@ -26,7 +89,8 @@ def go(infile, outstream, seed_set=ContainsEverything()):
             noun_match = noun_pattern.match(line)
             if noun_match:
                 head = noun_match.groups()[0]
-                for np_type, pattern in zip(['AN', 'NN'], [an_pattern, nn_pattern]):
+                for np_type, pattern in zip(['AN', 'NN'],
+                                            [an_modifier_feature_pattern, nn_modifier_feature_pattern]):
                     for modifier in pattern.findall(line):
                         if modifier in seed_set:
                             outstream.write('{}:{}_{}\n'.format(np_type, modifier, head))
@@ -40,6 +104,9 @@ def read_configuration():
     parser.add_argument('-s', '--modifier_set', required=False, type=str, default='',
                         help='Name of file containing a set of modifiers that will be considered. '
                              'All other ANs/NNs are disregarded.')
+    parser.add_argument('-v', '--vectors', action='store_true',
+                        help='If set, will also output '
+                             'window features for each entry occurrence .Default is False')
     return parser.parse_args()
 
 
@@ -50,7 +117,14 @@ if __name__ == '__main__':
     conf = read_configuration()
     output = conf.output if conf.output else '%s.ANsNNs' % conf.input
 
+    if conf.vectors:
+        logging.info('Will also output window features for each entry occurrence')
+        function = go_get_vectors
+    else:
+        logging.info('Will only output NP entries')
+        function = go
     seed = conf.modifier_set
+
     if seed:
         with open(seed) as f:
             seed = set(x.strip() for x in f.readlines())
@@ -58,7 +132,8 @@ if __name__ == '__main__':
     with open(output, 'w') as outstream:
         logging.info('Reading from %s', conf.input)
         logging.info('Writing to %s', output)
+
         if seed:
-            go(conf.input, outstream, seed_set=seed)
+            function(conf.input, outstream, seed_set=seed)
         else:
-            go(conf.input, outstream)
+            function(conf.input, outstream)
--- discoutils/tests/test_find_all_NPs.py
+++ discoutils/tests/test_find_all_NPs.py
@@ -1,5 +1,5 @@
 __author__ = 'miroslavbatchkarov'
-from discoutils.find_all_NPs import go
+from discoutils.find_all_NPs import go, go_get_vectors
 from cStringIO import StringIO
 
 
@@ -13,8 +13,8 @@ def test_find_all_NPs():
                "AN:fluffy/J_cat/N\n" \
                "NN:house/N_cat/N\n" \
                "NN:street/N_cat/N\n" \
-               "AN:troubled/J_activist/N\n"
-    print '\n', s.getvalue()
+               "AN:troubled/J_activist/N\n" \
+               "AN:leftw/J_activist/N\n"
     assert s.getvalue() == expected
 
 
@@ -22,5 +22,18 @@ def test_find_all_NPs_with_seed():
     s = StringIO()
     go('discoutils/tests/resources/exp10head.pbfiltered', s, seed_set={'ordinary/J'})
     expected = "AN:ordinary/J_session/N\n"
-    print '\n', s.getvalue()
     assert s.getvalue() == expected
+
+
+def test_find_all_NPs_with_window_vectors():
+    s = StringIO()
+    go_get_vectors('discoutils/tests/resources/exp10head.pbfiltered', s)
+    expected_features = {'T:something/N', 'T:large/J', 'T:what/CONJ', 'T:ever/N', 'T:feature/J'}
+    lines = s.getvalue().rstrip().split('\n')
+    from pprint import pprint
+
+    pprint(lines)
+    assert len(lines) == 7  # only some NPs have window features. Some lines in the test
+    # file mention multiple NPs- all need to be produced.
+    # Just check that the right features are returned, by do not bother with checking their ordering
+    assert expected_features == set(feature for line in lines for feature in line.split('\t')[1:])
