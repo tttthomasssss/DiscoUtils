fbbaf432641e443a55fd9e6324587c9bc41b324f
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-04-03 19:07:47 +0100

    Removed unnecessary information returned by PrecomputedSimilaritiesVectorSource.get_nearest_neighbours to speed it up by an order of magnitude. This breaks compatibility between its API and that of CompositeVectorSource, which is ok as the latt     (cherry picked from commit e54912b)

--- discoutils/tests/test_io_utils.py
+++ discoutils/tests/test_io_utils.py
@@ -5,7 +5,7 @@ import scipy.sparse as sp
 from discoutils.tokens import DocumentFeature
 from discoutils.tests.test_dimensionality_reduction import _read_and_strip_lines
 from discoutils.io_utils import write_vectors_to_disk
-from discoutils.tests.test_thesaurus import thesaurus_c
+from discoutils.tests.test_thesaurus import thesaurus_c # this is used, do not remove
 
 
 __author__ = 'mmb28'
@@ -13,9 +13,9 @@ __author__ = 'mmb28'
 
 @pytest.fixture(params=['normal', 'abbreviated', 'empty', 'with_filter', 'with_verb_only_filter'], scope='function')
 def resources(thesaurus_c, request):
-    filter_callable = lambda x: True  # accept and write all entries by default
+    filter_callable = lambda x: True
     features = ['a/N', 'b/V', 'd/J', 'g/N', 'x/X']
-    entries = [x.tokens_as_str() for x in sorted(thesaurus_c.keys())]
+    entries = list(sorted(thesaurus_c.keys()))
 
     if request.param == 'empty':
         thesaurus_c.clear()
@@ -32,8 +32,8 @@ def resources(thesaurus_c, request):
 
     if request.param == 'abbreviated':
         del thesaurus_c['g/N']
-        features.pop(-1)  # remove x/X
-        entries = [x.tokens_as_str() for x in sorted(thesaurus_c.keys())]
+        features.pop(-1) # remove x/X
+        entries = thesaurus_c.keys()
 
     return thesaurus_c, entries, features, filter_callable
 
@@ -50,12 +50,11 @@ def test_write_vectors_to_disk(resources, tmpdir):
     entries_file = str(tmpdir.join('entries.txt'))
     features_file = str(tmpdir.join('features.txt'))
 
-    matrix, cols, rows = th.to_sparse_matrix(row_transform=None, column_transform=None)
+    matrix, cols, rows = th.to_sparse_matrix()
+    rows = [DocumentFeature.from_string(x) for x in rows]
     write_vectors_to_disk(sp.coo_matrix(matrix), rows, cols,
                           events_file, features_file, entries_file,
-                          entry_filter=filter_callable,
-                          row_transform=DocumentFeature.tokens_as_str,
-                          column_transform=DocumentFeature.tokens_as_str)
+                          entry_filter=filter_callable)
 
     if expected_entries:
         # the file will not be written at all if there's nothing to put in it
--- discoutils/thesaurus_loader.py
+++ discoutils/thesaurus_loader.py
@@ -31,45 +31,15 @@ class Thesaurus(object):
         return getattr(self.d, name)
 
     def __setitem__(self, key, value):
-        if isinstance(key, str):
-            if isinstance(self.d, dict):
-                # given a string to store into a dict. make that into a document feature
-                key = DocumentFeature.from_string(key)
-        else:
-            if not isinstance(self.d, dict):
-                # given a document feature to shelve, make it into a string (shelf needs a string)
-                key = key.tokes_as_str()
-
         self.d[key] = value
 
     def __delitem__(self, key):
-        if isinstance(key, str):
-            key = DocumentFeature.from_string(key)
         del self.d[key]
 
     def __getitem__(self, item):
-        if isinstance(item, str):
-            neighbours = self.d[DocumentFeature.from_string(item)]
-            return [(x[0].tokens_as_str(), x[1]) for x in neighbours]
-        else:
-            if isinstance(self.d, dict):
-                # no shelving going on, look up directly
-                return self.d[item]
-            else:
-                # shelve keys must be string
-                return self.d[item.tokens_as_str()]
-
-    def get(self, key, default=None):
-        try:
-            return self[key]
-        except KeyError:
-            return default
+        return self.d[item]
 
     def __contains__(self, item):
-        if isinstance(item, str) and isinstance(self.d, dict):
-            item = DocumentFeature.from_string(item)
-        if isinstance(item, DocumentFeature) and not isinstance(self.d, dict):
-            item = item.tokens_as_str()
         return item in self.d
 
     def __len__(self):
@@ -95,7 +65,7 @@ class Thesaurus(object):
             will take place. This might be desirable when readings feature lists
         :type aggressive_lowercasing: bool
         :param ngram_separator: When n_gram entries are read in, what are the indidivual tokens separated by
-        :param vocabulary: a set of DocumentFeature-s. Features not contained in this set will be discarded.
+        :param vocabulary: a set of strings. Features not contained in this set will be discarded.
         :param allow_lexical_overlap: whether neighbours/features are allowed to overlap lexically with the entry
         they are neighbours/features of
         """
@@ -135,38 +105,24 @@ class Thesaurus(object):
             FILTERED = '___FILTERED___'.lower()
             with open(path) as infile:
                 for line in infile:
-                    entries = line.strip().split('\t')
-                    if len(entries) % 2 == 0:
+                    tokens = line.strip().split('\t')
+                    if len(tokens) % 2 == 0:
                         # must have an odd number of things, one for the entry
                         # and pairs for (neighbour, similarity)
                         logging.warn('Dodgy line in thesaurus file: %s\n %s', path, line)
                         continue
-                    if entries[0] != FILTERED:
-                        key = DocumentFeature.from_string(_smart_lower(entries[0],
-                                                                       ngram_separator,
-                                                                       aggressive_lowercasing))
-                        if key.type == 'EMPTY':
-                            # do not load things in the wrong format, they'll get in the way later
-                            logging.info('Skipping thesaurus entry %s', key)
-                            continue
-                        to_insert = []
-                        for (neighbour, sim) in walk_nonoverlapping_pairs(entries, 1):
-                            text = _smart_lower(neighbour, ngram_separator, aggressive_lowercasing)
-                            sim = float(sim)
-                            if text.lower() != FILTERED and sim > sim_threshold:
-                                # ensure filtered is removed
-                                # ensure min similirity requirement is met
-                                df = DocumentFeature.from_string(text)
-                                if df.type == 'EMPTY':
-                                    continue
+                    if tokens[0] != FILTERED:
+                        key = _smart_lower(tokens[0], ngram_separator, aggressive_lowercasing)
 
-                                if not allow_lexical_overlap and any(t in key.tokens for t in df.tokens):
-                                    # ignore potentially lexically overlapping neighbours
-                                    continue
-                                if df not in vocabulary:
-                                    # ignore neighbours not in the predefined vocabulary
-                                    continue
-                                to_insert.append((df, sim))
+                        to_insert = [(_smart_lower(word, ngram_separator, aggressive_lowercasing), float(sim))
+                                     for (word, sim) in walk_nonoverlapping_pairs(tokens, 1)
+                                     if word.lower() != FILTERED and word in vocabulary and float(sim) > sim_threshold]
+
+                        if not allow_lexical_overlap:
+                            features = [(DocumentFeature.from_string(x[0]), x[1]) for x in to_insert]
+                            key_tokens = DocumentFeature.from_string(key).tokens
+                            to_insert = [(f[0].tokens_as_str(), f[1]) for f in features
+                                         if not any(t in key_tokens for t in f[0].tokens)]
 
                         if include_self:
                             to_insert.insert(0, (key, 1.0))
@@ -174,9 +130,14 @@ class Thesaurus(object):
                         # the steps above may filter out all neighbours of an entry. if this happens,
                         # do not bother adding it
                         if len(to_insert) > 0:
+                            if DocumentFeature.from_string(key).type == 'EMPTY':
+                                # do not load things in the wrong format, they'll get in the way later
+                                logging.info('Skipping thesaurus entry %s', key)
+                                continue
+
                             if key in to_return:
                                 # todo this better not be a neighbours file, merging doesn't work there
-                                logging.warn('Multiple entries for "%s" found. Merging.' % entries[0])
+                                logging.warn('Multiple entries for "%s" found. Merging.' % tokens[0])
                                 c = Counter(dict(to_return[key]))
                                 c.update(dict(to_insert))
                                 to_return[key] = [(k, v) for k, v in c.iteritems()]
@@ -193,12 +154,12 @@ class Thesaurus(object):
         Uses the shelf module to persist this object to a file
         """
         logging.info('Shelving thesaurus of size %d to %s', len(self), filename)
-        d = shelve.open(filename, flag='c', writeback=True)  # read and write
+        d = shelve.open(filename, flag='c')  # read and write
         for entry, features in self.iteritems():
-            d[entry.tokens_as_str()] = features
+            d[str(entry)] = features
         d.close()
 
-    def to_dissect_sparse_files(self, output_prefix, row_transform=None, column_transform=None):
+    def to_dissect_sparse_files(self, output_prefix, row_transform=None):
         """
         Converting to a dissect sparse matrix format. Writes out 3 files
 
@@ -213,18 +174,15 @@ class Thesaurus(object):
             for entry in self.keys():
                 tmp_entry = row_transform(entry) if row_transform else entry
                 for feature, count in self[entry]:
-                    outfile.write('{} {} {}\n'.format(tmp_entry,
-                                                      column_transform(feature) if column_transform else feature,
-                                                      count))
+                    outfile.write('{} {} {}\n'.format(tmp_entry, feature, count))
 
         # write dissect columns file
         columns = set(feature for vector in self.values() for (feature, count) in vector)
         with open('{}.cols'.format(output_prefix), 'w+b') as outfile:
             for feature in sorted(columns):
-                outfile.write('{}\n'.format(column_transform(feature) if column_transform else feature))
+                outfile.write('{}\n'.format(feature))
 
-    def to_sparse_matrix(self, row_transform=DocumentFeature.tokens_as_str,
-                         column_transform=DocumentFeature.tokens_as_str, dtype=numpy.float):
+    def to_sparse_matrix(self, row_transform=None, dtype=numpy.float):
         """
         Converts the vectors held in this object to a scipy sparse matrix
         :return: a tuple containing
@@ -243,11 +201,8 @@ class Thesaurus(object):
         rows = [k for k in self.iterkeys()]
         if row_transform:
             rows = map(row_transform, rows)
-        cols = self.v.feature_names_
-        if column_transform:
-            cols = map(column_transform, cols)
 
-        return mat, cols, rows
+        return mat, self.v.feature_names_, rows
 
     def to_dissect_core_space(self):
         """
@@ -256,16 +211,9 @@ class Thesaurus(object):
         from composes.matrix.sparse_matrix import SparseMatrix
         from composes.semantic_space.space import Space
 
-        mat, cols, rows = self.to_sparse_matrix(row_transform=DocumentFeature.tokens_as_str,
-                                                column_transform=DocumentFeature.tokens_as_str)
+        mat, cols, rows = self.to_sparse_matrix()
         mat = SparseMatrix(mat)
         s = Space(mat, rows, cols)
-
-        # the dimensions of the semantic space are stored as DocumentFeature-s in self
-        # they need to be output as strings
-        old_voc = self.v.vocabulary_
-        transformed_voc = {DocumentFeature.tokens_as_str(k): v for k, v in old_voc.iteritems()}
-        self.v.vocabulary_ = transformed_voc
 
         # test that the mapping from string to its vector has not been messed up
         for i in range(min(10, len(self))):
@@ -273,12 +221,10 @@ class Thesaurus(object):
             s2 = self.v.transform(dict(self[rows[i]]))
             # sparse matrices do not currently support equality testing
             assert abs(s1 - s2).nnz == 0
-        self.v.vocabulary_ = old_voc
+
         return s
 
-    def to_file(self, filename, entry_filter=lambda x: True,
-                row_transform=DocumentFeature.tokens_as_str,
-                column_transform=DocumentFeature.tokens_as_str):
+    def to_file(self, filename, entry_filter=lambda x: True, row_transform=lambda x: x):
         """
         Writes this thesaurus to a Byblo-compatible events file like the one it was most likely read from. In the
         process converts all entries to a DocumentFeature.
@@ -290,9 +236,9 @@ class Thesaurus(object):
          directly convertible (needs to be african/J leader/N)
         :return: :rtype:
         """
-        mat, cols, rows = self.to_sparse_matrix(row_transform=None, column_transform=None)
-        write_vectors_to_disk(mat.tocoo(), rows, cols, filename, entry_filter=entry_filter,
-                              row_transform=row_transform, column_transform=column_transform)
+        mat, cols, rows = self.to_sparse_matrix(row_transform=row_transform)
+        rows = [DocumentFeature.from_string(x) for x in rows]
+        write_vectors_to_disk(mat.tocoo(), rows, cols, filename, entry_filter=entry_filter)
         return filename
 
 
