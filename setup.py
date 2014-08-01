ca4b01e522008367b8f36e4cc19707f6f1dbe4a6
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-08-01 11:19:49 +0100

    Added test command to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 from Cython.Build import cythonize
+import pytest
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = 'discoutils/tests'
+        self.test_suite = True
+
+    def run_tests(self):
+        pytest.main(self.test_args)
 
 setup(
     name='DiscoUtils',
@@ -8,6 +20,8 @@ setup(
     packages=['discoutils', 'discoutils.tests'],
     author=['Julie Weeds', 'Miroslav Batchkarov'],
     author_email=['J.E.Weeds@sussex.ac.uk', 'M.Batchkarov@sussex.ac.uk'],
+    tests_require=['pytest>=2.4.2'],
+    cmdclass={'test': PyTest},
     install_requires=['pytest', 'Cython', 'iterpipes', 'numpy', 'scipy', 'scikit-learn'],
     ext_modules=cythonize(["discoutils/tokens.pyx"])
 )
