3dcb9d88c05d57a14f201cee905c09acb254e61a
Miroslav Batchkarov <mbatchkarov@gmail.com>
2014-11-04 13:52:27 +0000

    explicit no gzip

--- discoutils/tests/test_thesaurus.py
+++ discoutils/tests/test_thesaurus.py
@@ -387,7 +387,7 @@ def test_max_num_neighbours_and_no_lexical_overlap():
 
 
 def test_loading_from_tar():
-    t1 = Thesaurus.from_tsv('discoutils/tests/resources/exp0-0a.strings')
+    t1 = Thesaurus.from_tsv('discoutils/tests/resources/exp0-0a.strings', gzipped=False)
     t2 = Thesaurus.from_tsv('discoutils/tests/resources/exp0-0a.strings.gz', gzipped=True)
     for k, v in t1.items():
         assert k in t2
